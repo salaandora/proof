af9ffa3a00ae6385309e93bd0dcc56998a02dfb6
salaandora <50085010+salaandora@users.noreply.github.com>
2020-07-08 22:41:02 -0400

    Update twoplustwoproof.py

--- twoplustwoproof.py
+++ twoplustwoproof.py
@@ -1,8 +1,8 @@
 #Defines two sets each with two elements
 a = ['b', 'c']
 d = ['e', 'f']
-#Creates a union of a, b within set a
+#Creates the intersection of a, b within set a
 for let in d:
     a += let
-#Prints length of set a, equivalent to the union of a, b
+#Prints length of set a, equivalent to the intersection of a, b
 print (len(a))
