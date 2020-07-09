57b5112f708c2217fd92dbb2cb1c07213961f3df
salaandora <50085010+salaandora@users.noreply.github.com>
2020-07-08 22:38:18 -0400

    Update twoplustwoproof.py

--- twoplustwoproof.py
+++ twoplustwoproof.py
@@ -1,3 +1,8 @@
-one = 1
-proof = one+one
-print (proof)
+#Defines two sets each with two elements
+a = ['b', 'c']
+d = ['e', 'f']
+#Creates a union of a, b within set a
+for let in d:
+    a += let
+#Prints length of set a, equivalent to the union of a, b
+print (len(a))
